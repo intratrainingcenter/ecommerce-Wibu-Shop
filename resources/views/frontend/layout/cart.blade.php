21710e26b348333369e0c7b86cec3e95804810b9
oky pratama <okypratama5758@gmail.com>
2018-10-16 10:20:55 +0700

    /test_oneSignal: /messageFronend:

---
+++ resources/views/frontend/layout/cart.blade.php
@@ -0,0 +1,53 @@
+<div class="top-cart-block">
+  <div class="top-cart-info">
+    <!-- Chat -->
+    <a href="#chat-pop-up" class="top-cart-info-count fancybox-fast-view">Chat</a>
+  </div>
+  <a href="#chat-pop-up" class="top-cart-info-count fancybox-fast-view"> <i class="fa fa-comments-o"></i></a>
+</div>
+
+<div class="top-cart-block">
+  <div class="top-cart-info">
+    <a href="javascript:void(0);" class="top-cart-info-count">3 items</a>
+    <a href="javascript:void(0);" class="top-cart-info-value">$1260</a>
+  </div>
+  <i class="fa fa-shopping-cart"></i>
+  <div class="top-cart-content-wrapper">
+    <div class="top-cart-content">
+      <ul class="scroller" style="height: 250px;">
+        <li>
+          <a href="shop-item.html"><img src="{{asset('frontend/theme/assets/pages/img/cart-img.jpg')}}" alt="Rolex Classic Watch" width="37" height="34"></a>
+          <span class="cart-content-count">x 1</span>
+          <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
+          <em>$1230</em>
+          <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
+        </li>
+        <li>
+          <a href="shop-item.html"><img src="{{asset('frontend/theme/assets/pages/img/cart-img.jpg')}}" alt="Rolex Classic Watch" width="37" height="34"></a>
+          <span class="cart-content-count">x 1</span>
+          <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
+          <em>$1230</em>
+          <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
+        </li>
+        <li>
+          <a href="shop-item.html"><img src="{{asset('frontend/theme/assets/pages/img/cart-img.jpg')}}" alt="Rolex Classic Watch" width="37" height="34"></a>
+          <span class="cart-content-count">x 1</span>
+          <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
+          <em>$1230</em>
+          <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
+        </li>
+        <li>
+          <a href="shop-item.html"><img src="{{asset('frontend/theme/assets/pages/img/cart-img.jpg')}}" alt="Rolex Classic Watch" width="37" height="34"></a>
+          <span class="cart-content-count">x 1</span>
+          <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
+          <em>$1230</em>
+          <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
+        </li>
+      </ul>
+      <div class="text-right">
+        <a href="shop-shopping-cart.html" class="btn btn-default">View Cart</a>
+        <a href="shop-checkout.html" class="btn btn-primary">Checkout</a>
+      </div>
+    </div>
+  </div>
+</div>
