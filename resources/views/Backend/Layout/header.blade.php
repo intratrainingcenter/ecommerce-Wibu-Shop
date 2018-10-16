7df9f690dc5cf1f773d9d4549694a6d6fa67cc64
jojo <joniliwong69@gmail.com>
2018-10-16 11:02:27 +0700

    kategori fix

--- resources/views/Backend/Layout/header.blade.php
+++ resources/views/Backend/Layout/header.blade.php
@@ -202,7 +202,7 @@
         <li class="header">MENU</li>
         @if (auth::user()->jabatan == 'Admin')
           <li><a href="{{route('dashboard')}}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-          <li><a href=""><i class="fa fa-files-o"></i> Data Kategori</a></li>
+          <li><a href="{{route('kategori')}}"><i class="fa fa-files-o"></i> Data Kategori</a></li>
           <li><a href=""><i class="fa fa-th"></i> <span>Data Produk</span></a></li>
           <li><a href=""><i class="fa fa-pie-chart"></i><span>Data Pembeli</span></a></li>
           <li><a href=""><i class="fa fa-book"></i><span>Data Promo</span></a></li>
@@ -211,7 +211,7 @@
           <li><a href=""><i class="fa fa-edit"></i> <span>Data Message</span></a></li>
         @elseif (auth::user()->jabatan == 'Spv')
           <li><a href="{{route('dashboard')}}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-          <li><a href=""><i class="fa fa-files-o"></i> Data Kategori</a></li>
+          <li><a href="{{route('kategori')}}"><i class="fa fa-files-o"></i> Data Kategori</a></li>
           <li><a href=""><i class="fa fa-th"></i> <span>Data Produk</span></a></li>
           <li><a href=""><i class="fa fa-pie-chart"></i><span>Data Pembeli</span></a></li>
           <li><a href=""><i class="fa fa-book"></i><span>Data Promo</span></a></li>
