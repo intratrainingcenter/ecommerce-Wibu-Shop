c21a40cc0e3dafdea3aee020d657e17cd4e9ca79
jojo <joniliwong69@gmail.com>
2018-10-18 15:19:26 +0700

    /laporanBarang: Auto stash before merge of "feature/laporanBarang" and "develop"

--- app/Http/Controllers/laporanBarang.php
+++ app/Http/Controllers/laporanBarang.php
@@ -8,39 +8,154 @@ use Illuminate\Support\Facades\DB;
 class laporanBarang extends Controller
 {
   public function Index() {
+    $category = DB::table('kategoris')->get();
+    // dd($category);
     $date = date('Y-m-d');
     $data = DB::table('produks')
-                   ->join('transaksi_pembelians','transaksi_pembelians.kode_produk','=','produks.kode_produk')
-                   ->join('transaksi_penjualans','transaksi_penjualans.kode_produk','=','produks.kode_produk')
-                   // ->selectRaw('sum(sub_total) as total')
-                   // ->where('kode_transaksi_pembelian', 'like', 'TrRb%')
-                   // ->where('created_at', $date)
+                   ->join('kategoris','kategoris.kode_kategori','=','produks.kode_kategori')
+                   ->leftJoin('transaksi_pembelians','transaksi_pembelians.kode_produk','=','produks.kode_produk')
+                   ->leftJoin('transaksi_penjualans','transaksi_penjualans.kode_produk','=','produks.kode_produk')
+                   ->select('produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                              'produks.nama_produk','produks.hpp','produks.harga',
+                              'kategoris.kode_kategori','kategoris.nama_kategori',
+                              DB::raw('SUM(produks.stok) as stock'),DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'),DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                   ->groupBy('produks.id','produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                              'produks.nama_produk','produks.hpp','produks.harga',
+                              'kategoris.kode_kategori','kategoris.id','kategoris.nama_kategori')
+                   ->orderBy('produks.id')
                    ->get();
-                   // dd($data);
-    // $Buy = DB::table('transaksi_penjualans')
-    //                ->selectRaw('sum(grand_total) as total')
-    //                ->where('kode_transaksi_penjualan', 'like', 'TrR%')
-    //                ->where('created_at', $date)
-    //                ->first();
-    // $untung = $Shell>$Buy;
-    // $Rugi = $Shell<$Buy;
-    return view('Backend.LaporanBarang.general',compact('data'));
+    $Shell = DB::table('transaksi_penjualans')
+                  ->select('transaksi_penjualans.kode_produk',DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'))
+                  ->groupBy('kode_produk')->orderBy('kode_produk')
+                  ->get();
+    $Buy = DB::table('transaksi_pembelians')
+                  ->select('transaksi_pembelians.kode_produk',DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                  ->groupBy('kode_produk')->orderBy('kode_produk')
+                  ->get();
+    $groupProduct = DB::table('produks')->select('kode_produk')->groupBy('id')->get();
+    // dd($groupProduct);
+    return view('Backend.LaporanBarang.general',compact('data','category','groupProduct','Buy','Shell'));
   }
   public function Filter(Request $request) {
-    // $start = $request->dari;
-    // $finis = $request->sampai;
-    // $Shell = DB::table('transaksi_pembelians')
-    //                ->selectRaw('sum(sub_total) as total')
-    //                ->where('kode_transaksi_pembelian', 'like', 'TrRb%')
-    //                ->whereBetween('created_at', [$start, $finis])
-    //                ->first();
-    // $Buy = DB::table('transaksi_penjualans')
-    //                ->selectRaw('sum(grand_total) as total')
-    //                ->where('kode_transaksi_penjualan', 'like', 'TrR%')
-    //                ->whereBetween('created_at', [$start, $finis])
-    //                ->first();
-    // $untung = $Shell>$Buy;
-    // $Rugi = $Shell<$Buy;
-    // return view('Backend.LaporanKeuangan.general',compact(['Shell','Buy']));
+    $codecategory = $request->kode_kategori;
+    // dd($codecategory);
+    $start = $request->dari;
+    $finish = $request->sampai;
+    if ($codecategory == 'ada' && $start == null && $finish == null) {
+      // dd('ada');
+      $category = DB::table('kategoris')->get();
+      $date = date('Y-m-d');
+      $data = DB::table('produks')
+                     ->join('kategoris','kategoris.kode_kategori','=','produks.kode_kategori')
+                     ->leftJoin('transaksi_pembelians','transaksi_pembelians.kode_produk','=','produks.kode_produk')
+                     ->leftJoin('transaksi_penjualans','transaksi_penjualans.kode_produk','=','produks.kode_produk')
+                     ->select('produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                                'produks.nama_produk','produks.hpp','produks.harga',
+                                'kategoris.kode_kategori','kategoris.nama_kategori',
+                                DB::raw('SUM(produks.stok) as stock'),DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'),DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                     ->groupBy('produks.id','produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                                'produks.nama_produk','produks.hpp','produks.harga',
+                                'kategoris.kode_kategori','kategoris.id','kategoris.nama_kategori')
+                     ->orderBy('produks.id')
+                     ->get();
+      $Shell = DB::table('transaksi_penjualans')
+                    ->select('transaksi_penjualans.kode_produk',DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'))
+                    ->groupBy('kode_produk')->orderBy('kode_produk')
+                    ->get();
+      $Buy = DB::table('transaksi_pembelians')
+                    ->select('transaksi_pembelians.kode_produk',DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                    ->groupBy('kode_produk')->orderBy('kode_produk')
+                    ->get();
+      $groupProduct = DB::table('produks')->select('kode_produk')->groupBy('id')->get();
+      // dd($groupProduct);
+      return view('Backend.LaporanBarang.general',compact('data','category','groupProduct','Buy','Shell'));
+    }elseif ($start == null && $finish == null) {
+      $category = DB::table('kategoris')->get();
+
+      $date = date('Y-m-d');
+      $data = DB::table('produks')
+                     ->join('kategoris','kategoris.kode_kategori','=','produks.kode_kategori')
+                     ->leftJoin('transaksi_pembelians','transaksi_pembelians.kode_produk','=','produks.kode_produk')
+                     ->leftJoin('transaksi_penjualans','transaksi_penjualans.kode_produk','=','produks.kode_produk')
+                     ->select('produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                                'produks.nama_produk','produks.hpp','produks.harga',
+                                'kategoris.kode_kategori','kategoris.nama_kategori',
+                                DB::raw('SUM(produks.stok) as stock'),DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'),DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                     ->groupBy('produks.id','produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                                'produks.nama_produk','produks.hpp','produks.harga',
+                                'kategoris.kode_kategori','kategoris.id','kategoris.nama_kategori')
+                     ->orderBy('produks.id')
+                     ->where('produks.kode_kategori','=',$codecategory)
+                     ->get();
+      $Shell = DB::table('transaksi_penjualans')
+                    ->select('transaksi_penjualans.kode_produk',DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'))
+                    ->groupBy('kode_produk')->orderBy('kode_produk')
+                    ->get();
+      $Buy = DB::table('transaksi_pembelians')
+                    ->select('transaksi_pembelians.kode_produk',DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                    ->groupBy('kode_produk')->orderBy('kode_produk')
+                    ->get();
+      $groupProduct = DB::table('produks')->select('kode_produk')->groupBy('id')->get();
+      // dd($groupProduct);
+      return view('Backend.LaporanBarang.general',compact('data','category','groupProduct','Buy','Shell'));
+    }else {
+      if ($category == 'ada') {
+        dd('ada ada');
+        // code...
+        $data = DB::table('produks')
+              ->join('kategoris','kategoris.kode_kategori','=','produks.kode_kategori')
+              ->leftJoin('transaksi_pembelians','transaksi_pembelians.kode_produk','=','produks.kode_produk')
+              ->leftJoin('transaksi_penjualans','transaksi_penjualans.kode_produk','=','produks.kode_produk')
+              ->select('produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                        'produks.nama_produk','produks.hpp','produks.harga',
+                        'kategoris.kode_kategori','kategoris.nama_kategori',
+                        DB::raw('SUM(produks.stok) as stock'),DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'),DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+              ->groupBy('produks.id','produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                        'produks.nama_produk','produks.hpp','produks.harga',
+                        'kategoris.kode_kategori','kategoris.id','kategoris.nama_kategori')
+              ->orderBy('produks.id')
+              ->whereBetween('produks.created_at',[$start,$finish])
+              ->get();
+        $category = DB::table('kategoris')->get();
+        $Shell = DB::table('transaksi_penjualans')
+                      ->select('transaksi_penjualans.kode_produk',DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'))
+                      ->groupBy('kode_produk')->orderBy('kode_produk')
+                      ->get();
+        $Buy = DB::table('transaksi_pembelians')
+                      ->select('transaksi_pembelians.kode_produk',DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                      ->groupBy('kode_produk')->orderBy('kode_produk')
+                      ->get();
+        $groupProduct = DB::table('produks')->select('kode_produk')->groupBy('kode_produk')->get();
+        return view('Backend.LaporanBarang.general',compact('data','category','groupProduct','Buy','Shell'));
+      }else {
+      dd('tidak ada ada');
+      $data = DB::table('produks')
+                ->join('kategoris','kategoris.kode_kategori','=','produks.kode_kategori')
+                ->leftJoin('transaksi_pembelians','transaksi_pembelians.kode_produk','=','produks.kode_produk')
+                ->leftJoin('transaksi_penjualans','transaksi_penjualans.kode_produk','=','produks.kode_produk')
+                ->select('produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                        'produks.nama_produk','produks.hpp','produks.harga',
+                        'kategoris.kode_kategori','kategoris.nama_kategori',
+                        DB::raw('SUM(produks.stok) as stock'),DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'),DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                ->groupBy('produks.id','produks.kode_produk','produks.nama_produk','produks.kode_kategori',
+                        'produks.nama_produk','produks.hpp','produks.harga',
+                        'kategoris.kode_kategori','kategoris.id','kategoris.nama_kategori')
+                ->orderBy('produks.id')
+                ->where('produks.kode_kategori','=',$category)
+                ->whereBetween('produks.created_at',[$start,$finish])
+                ->get();
+      $category = DB::table('kategoris')->get();
+      $Shell = DB::table('transaksi_penjualans')
+                  ->select('transaksi_penjualans.kode_produk',DB::raw('SUM(transaksi_penjualans.jumlah) as keluar'))
+                  ->groupBy('kode_produk')->orderBy('kode_produk')
+                  ->get();
+      $Buy = DB::table('transaksi_pembelians')
+                ->select('transaksi_pembelians.kode_produk',DB::raw('SUM(transaksi_pembelians.jummlah) as masuk'))
+                ->groupBy('kode_produk')->orderBy('kode_produk')
+                ->get();
+      $groupProduct = DB::table('produks')->select('kode_produk')->groupBy('kode_produk')->get();
+      return view('Backend.LaporanBarang.general',compact('data','category','groupProduct','Buy','Shell'));
+    }
+    }
   }
 }
--- resources/views/Backend/LaporanBarang/general.blade.php
+++ resources/views/Backend/LaporanBarang/general.blade.php
@@ -10,8 +10,18 @@
 	      <div class="col-xs-12">
 	        <div class="box">
 									<div class="panel-body">
-										<form action="{{route('FilterLaporanTransaksi')}}">
-												<div class="col-lg-4">
+										<form action="{{route('FilterLaporanProduct')}}" >
+											<div class="col-lg-2">
+												<div class="form-group"  style="width: 100px;">
+													<select class="btn btn-3d" name="kode_kategori" id="kategorinya">
+															<option value="ada">Semua kategori</option>
+														@foreach ($category as $key)
+														<option  value="{{$key->kode_kategori}}">{{$key->nama_kategori}}</option>
+														@endforeach
+													</select>
+												</div>
+											</div>
+												{{-- <div class="col-lg-4">
 													<div class="form-group"  style="width: 100px;">
 														<div class="input-group">
 															<div class="input-group-addon"><i class="fa fa-calendar"></i></div>
@@ -26,12 +36,14 @@
 															<input type="datetime-local" class="form-control pull-right"  name="sampai" >
 														</div>
 													</div>
-												</div>
+												</div> --}}
 												<div class="col-lg-4 right">
 													<button class="btn btn-primary btn-3d">Filter</button>
 													<a href="#" onclick="javascript:printlayer('print_lpTransaksi')" class="btn btn-primary btn-3d pull-right"><i class="fa fa-print"></i> Print</a>
 												</div>
 										</form>
+
+
 										<div id="print_lpTransaksi" class="col-md-12" style="margin-top:20px;">
 											<div class="panel">
 												<div class="panel-heading">
@@ -43,21 +55,64 @@
 															<th>#</th>
 															<th>kode Product</th>
 															<th>nama Product</th>
+															<th>Hpp</th>
+															<th>Harga Jual</th>
+															<th>stok awal</th>
 															<th>Masuk</th>
 															<th>keluar</th>
 															<th>stok Yang tersisa</th>
 														</tr>
 														<tbody>
-															@foreach ($data as $key => $value)
+												@foreach ($category as $key => $value)
+																<tr>
+																	<td colspan="9">{{$value->nama_kategori}}</td>
+																</tr>
+																@php
+																	$row = 0;
+																	$no = 1;
+																	$kode_tt=null;
+																	$kode_tt2=null;
+																@endphp
+																@php($nama = '')
+													@foreach ($data as $key => $valuedata)
+														@if ($valuedata->kode_kategori == $value->kode_kategori)
 															<tr>
-																<td>{{$key+1}}</td>
-																<td>{{$value->kode_produk}}</td>
-																<td>{{$value->nama_produk}}</td>
-																<td>{{$value->jumlah}}</td>
-																<td>{{$value->jummlah}}</td>
-																<td>a</td>
+																@if($nama == $valuedata->kode_produk) {{-- 3 --}}@else
+																	@php($row++)
+																	@php($kode_tt=$valuedata->kode_produk)
+																	@php($kode_tt2=$valuedata->kode_produk)
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">{{$no++}}</td>
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">{{$valuedata->kode_produk}}</td>
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">{{$valuedata->nama_produk}}</td>
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">{{"Rp. ".number_format($valuedata->hpp)}}</td>
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">{{"Rp. ".number_format($valuedata->harga)}}</td>
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">{{$valuedata->stock - $valuedata->masuk}}</td>
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">
+																	@if ($Buy->whereIn('kode_produk',[$valuedata->kode_produk])->isNotEmpty())
+																		@foreach ($Buy as $key => $valueBuy)
+																			@if ($valueBuy->kode_produk == $valuedata->kode_produk)
+																				{{$valueBuy->masuk}}
+																			@endif
+																		@endforeach
+																	@else -
+																	@endif
+																</td>
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">
+																	@if ($Shell->whereIn('kode_produk',[$valuedata->kode_produk])->isNotEmpty())
+																		@foreach ($Shell as $key => $valueShell)
+																			@if ($valueShell->kode_produk == $valuedata->kode_produk)
+																				{{$valueShell->keluar}}
+																			@endif
+																		@endforeach
+																	@else -
+																	@endif
+																</td>
+																<td rowspan="{{ $row }}" style="vertical-align : middle;text-align:center;">{{$valuedata->stock}}</td>
 															</tr>
-														@endforeach
+																@endif
+																@endif
+													@endforeach
+												@endforeach
 														</tbody>
 													</table>
 												</div>
