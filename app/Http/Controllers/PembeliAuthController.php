c8f4df849054449c8c9094239dede9aaa91b9fd5
Mujib Ghufron <mujibghufron.a7x@gmail.com>
2018-10-18 14:37:50 +0700

    fixing error

--- app/Http/Controllers/PembeliAuthController.php
+++ app/Http/Controllers/PembeliAuthController.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use App\TransaksiPenjualan as Penjualan;
 use App\PembeliAuth as Pembeli;
 use App\Kategori;
 use App\Produk;
 use App\Alamat;
-use App\TransaksiPenjualan as Penjualan;
 use Validator;
 
 class PembeliAuthController extends Controller
@@ -18,19 +18,19 @@ class PembeliAuthController extends Controller
 
     public function index()
     {
-        $id = Auth::guard('pembeli')->id();
-        $user = Pembeli::where('id', $id)->first();
-        $kategori = Kategori::all();
-        $all_products = Produk::orderBy('created_at','desc')->get();
-        $new_products = Produk::limit(4)->orderBy('created_at','desc')->get();
-        $point = Penjualan::where('kode_pembeli', $user->kode_pembeli)->count();
-        $orders = Penjualan::where('kode_pembeli', $user->kode_pembeli)->limit(3)->get();
+        $id             = Auth::guard('pembeli')->id();
+        $user           = Pembeli::where('id', $id)->first();
+        $kategori       = Kategori::all();
+        $all_products   = Produk::orderBy('created_at','desc')->get();
+        $new_products   = Produk::limit(4)->orderBy('created_at','desc')->get();
+        $point          = Penjualan::where('kode_pembeli', $user->kode_pembeli)->count();
+        $orders         = Penjualan::where('kode_pembeli', $user->kode_pembeli)->limit(3)->get();
         return view('frontend.pages.account.index',compact('user', 'point', 'orders', 'kategori', 'new_products', 'all_products'));
     }
 
     public function showRegisterForm()
     {
-      $kategori = Kategori::all();
+      $kategori     = Kategori::all();
       $all_products = Produk::orderBy('created_at','desc')->get();
       $new_products = Produk::limit(4)->orderBy('created_at','desc')->get();
         return view('frontend.pages.auth.register',compact('kategori','all_products', 'new_products'));
@@ -38,22 +38,22 @@ class PembeliAuthController extends Controller
 
     public function Register(Request $request)
     {
-        $getMaxID = Pembeli::max('id') + 1;
-        $getCode = 'PMB' . date('ymdhis') . $getMaxID;
-        $register = Pembeli::create([
-            'kode_pembeli' => $getCode,
-            'nama_pembeli' => $request->nama,
+        $getMaxID   = Pembeli::max('id') + 1;
+        $getCode    = 'PMB' . date('ymdhis') . $getMaxID;
+        $register   = Pembeli::create([
+            'kode_pembeli'  => $getCode,
+            'nama_pembeli'  => $request->nama,
             'jenis_kelamin' => $request->jenis_kelamin,
-            'email'        => $request->email,
-            'password'     => Hash::make($request->pass),
-            'foto'         => '',
+            'email'         => $request->email,
+            'password'      => Hash::make($request->pass),
+            'foto'          => '',
         ]);
         return redirect()->route('pembeli.login');
     }
 
     public function showLoginForm()
     {
-      $kategori = Kategori::all();
+      $kategori     = Kategori::all();
       $all_products = Produk::orderBy('created_at','desc')->get();
       $new_products = Produk::limit(4)->orderBy('created_at','desc')->get();
       return view('frontend.pages.auth.login',compact('kategori','all_products', 'new_products'));
@@ -61,8 +61,8 @@ class PembeliAuthController extends Controller
 
     public function Login(Request $request)
     {
-        $user = Pembeli::where('email', $request->email)->first();
-        $check = Auth::guard('pembeli')->attempt(request(['email', 'password']));
+        $user   = Pembeli::where('email', $request->email)->first();
+        $check  = Auth::guard('pembeli')->attempt(request(['email', 'password']));
         if ( $check ) {
             Auth::guard('pembeli')->login($user);
             return redirect('/');
@@ -76,27 +76,26 @@ class PembeliAuthController extends Controller
         if (Auth::guard('pembeli')->check()) {
             Auth::guard('pembeli')->logout();
         }
-
         return redirect()->route('pembeli.login');
     }
 
     public function Edit()
     {
-        $kategori = Kategori::all();
-        $all_products = Produk::orderBy('created_at','desc')->get();
-        $new_products = Produk::limit(4)->orderBy('created_at','desc')->get();
-        $id = Auth::guard('pembeli')->id();
-        $user = Pembeli::where('id', $id)->first();
+        $kategori       = Kategori::all();
+        $all_products   = Produk::orderBy('created_at','desc')->get();
+        $new_products   = Produk::limit(4)->orderBy('created_at','desc')->get();
+        $id             = Auth::guard('pembeli')->id();
+        $user           = Pembeli::where('id', $id)->first();
         return view('frontend.pages.account.editaccount', compact('user', 'alamat', 'kategori', 'all_products', 'new_products'));
     }
 
     public function EditPassword()
     {
-        $kategori = Kategori::all();
-        $all_products = Produk::orderBy('created_at','desc')->get();
-        $new_products = Produk::limit(4)->orderBy('created_at','desc')->get();
-        $id = Auth::guard('pembeli')->id();
-        $user = Pembeli::where('id', $id)->first();
+        $kategori       = Kategori::all();
+        $all_products   = Produk::orderBy('created_at','desc')->get();
+        $new_products   = Produk::limit(4)->orderBy('created_at','desc')->get();
+        $id             = Auth::guard('pembeli')->id();
+        $user           = Pembeli::where('id', $id)->first();
         return view('frontend.pages.account.change_password', compact('user', 'kategori', 'all_products', 'new_products'));
     }
 
@@ -112,12 +111,12 @@ class PembeliAuthController extends Controller
             return redirect()->back()->with('alertfail', 'Gagal');
         } else {
             if ($request->hasFile('foto')) {
-                $get_pembeli       = Pembeli::where('id', $id)->first();
+                $get_pembeli    = Pembeli::where('id', $id)->first();
                 Storage::delete($get_pembeli->foto);
                 $foto           = $request->foto;  
                 $GetExtension   = $foto->getClientOriginalExtension();
                 $path           = $foto->storeAs('public/images', $get_pembeli->kode_pembeli . '.' . $GetExtension);
-                $update       = Pembeli::where('id', $id)->update([
+                $update         = Pembeli::where('id', $id)->update([
                     'nama_pembeli'  => $request->nama_pembeli,
                     'jenis_kelamin' => $request->jenis_kelamin,
                     'foto'          => $path,
@@ -139,8 +138,8 @@ class PembeliAuthController extends Controller
         $validator = Validator::make($request->all(), [
             'new_password'  => 'required|max:12',
         ]);
-        $pembeli = Pembeli::where('id', $id)->first();
-        $check = Hash::check($request->password, $pembeli->password);
+        $pembeli   = Pembeli::where('id', $id)->first();
+        $check     = Hash::check($request->password, $pembeli->password);
         if ($validator->fails()) {
             return redirect()->back()->with('alertfail', 'Gagal');
         } else {
