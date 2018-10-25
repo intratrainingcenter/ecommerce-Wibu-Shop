816856818013e05d41d0feebbae1d2c53063072f
oky pratama <okypratama5758@gmail.com>
2018-10-25 13:34:56 +0700

    UI MessagesBackend

---
+++ resources/views/Backend/Messages/additional.blade.php
@@ -0,0 +1,9 @@
+@section('header')
+<link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
+<link rel="stylesheet" href="{{asset('css/Chat_Backend.css')}}">
+@endsection
+
+@section('footer')
+<script src="{{asset('js/Chat_Backend.js')}}" charset="utf-8"></script>
+@endsection
